89453b118d922de04ee3ea38f053d1a99bbf279c
phamgialinhlx <79445383+phamgialinhlx@users.noreply.github.com>
2021-09-26 21:53:17 +0700

    cmd ver1
    Phiên bản từ điển command line cải tiến lần 1.

---
+++ Dictionary.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Dictionary {
+    ArrayList<Word> words = new ArrayList<>();
+
+    public void addNewWord(String wordTarget, String wordExplain) {
+        words.add(new Word(wordTarget, wordExplain));
+    }
+
+    public int numberOrWords() {
+        return words.size();
+    }
+
+    public String getWordTarget(int index) {
+        return words.get(index).getWordTarget();
+    }
+
+    public String getWordExplain(int index) {
+        return words.get(index).getWordExplain();
+    }
+}
